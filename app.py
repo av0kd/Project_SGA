647fe318c8f6dede1178cb45a4670cbc8187ba4a
Pedro Henrique <111392329+pHNeutro14@users.noreply.github.com>
2025-02-07 20:10:02 +0000

    atualização nas rotas de criar turmas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from models import db, Turma, Aluno, Disciplina, Nota
 
 
@@ -10,6 +10,42 @@ db.init_app(app)
 @app.route('/')
 def inicio():
     return render_template('index.html')
+
+#Rotas para gerenciar a parte dos alunos
+@app.route('/adicionar_alunos')
+def adicionar_alunos():
+    return render_template('adicionar_alunos.html')
+
+@app.route('/alunos_adicionados', methods=["POST"])
+def alunos_adicionados():
+    nome = request.form.get('nome')
+    matricula = request.form.get('matricula')
+    turma_id = request.form.get('turma_id')
+
+    novo_aluno = Aluno(nome=nome, matricula=matricula, turma_id=turma_id)
+    db.session.add(novo_aluno)
+    db.session.commit()
+    return render_template('alunos_adicionados.html')
+
+#Rotas para gerenciar a parte das turmas
+@app.route('/criar_turmas')
+def criar_turmas():
+    return render_template('criar_turmas.html')
+
+@app.route('/turmas_criadas', methods=["POST"])
+def turmas_criadas():
+    nome_turma = request.form.get('nome_turma')
+
+    # Verificação simples para evitar turmas sem nome
+    if not nome_turma:
+        return "Erro: Nome da turma é obrigatório.", 400
+
+    # Criação da nova turma
+    nova_turma = Turma(nome=nome_turma)
+    db.session.add(nova_turma)
+    db.session.commit()
+
+    return render_template('turmas_criadas.html', turma=nova_turma)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- init_db.py
+++ init_db.py
@@ -4,3 +4,6 @@ from models import db
 with app.app_context():
     db.create_all()
     print("funfou as tabelas mrm")
+
+
+#banco ja criado, não precisa rodar novamente este código
